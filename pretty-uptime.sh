2f8b77b81376e64f731d31e19f7b415ce734b1db
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2018-04-01 12:09:54 +0530

    Update pretty-uptime.sh

--- pretty-uptime.sh
+++ pretty-uptime.sh
@@ -5,5 +5,5 @@ secs=$((upSeconds%60))
 mins=$((upSeconds/60%60))
 hours=$((upSeconds/3600%24))
 days=$((upSeconds/86400))
-UPTIME=$(printf "%d days, %02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs")
+UPTIME=$(printf "%dd%02dh%02dm%02ds" "$days" "$hours" "$mins" "$secs")
 echo $UPTIME
