56af33bb512ce4a9d44dbe23e59618642f5b3b29
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2020-02-12 16:02:00 +0530

    Download powerline config to ~/.config/powerline

--- powerline/ipython_config.py
+++ powerline/ipython_config.py
@@ -1,4 +1,3 @@
-from powerline.bindings.ipython.since_5 import PowerlinePrompts
-c = get_config()
+from powerline.bindings.ipython.since_7 import PowerlinePrompts
 c.TerminalInteractiveShell.simple_prompt = False
 c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
