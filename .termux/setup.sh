e0660e2d8759a02bf4fc4c3f98bac63ca5e37f54
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2020-02-13 17:27:41 +0530

    Use Inconsolota font on Termux

--- .termux/setup.sh
+++ .termux/setup.sh
@@ -4,7 +4,7 @@ function install() {
     echo "Installing powerline fonts"
     FONTPATH=~/.termux
     mkdir -p $FONTPATH
-    curl https://raw.githubusercontent.com/powerline/fonts/master/LiberationMono/Literation%20Mono%20Powerline.ttf -o $FONTPATH/font.ttf
+    curl https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/InconsolataGo/Regular/complete/InconsolataGo%20Nerd%20Font%20Complete.ttf -o $FONTPATH/font.ttf
     termux-reload-settings
 
     echo "Setting up termux-url-opener for downloading tracks"
