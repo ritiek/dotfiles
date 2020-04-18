65030a180e7ddbeb9ed95c53fa0b70fa4cfe2d72
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2020-04-19 01:51:25 +0530

    pip install psutil

--- setup.sh
+++ setup.sh
@@ -111,6 +111,7 @@ function install() {
 
 
         pip3 install dbus-python --user -U
+        pip3 install psutil --user -U
         pip3 install git+https://github.com/powerline/powerline.git --user -U
         pip3 install powerline-gitstatus --user -U
         echo
