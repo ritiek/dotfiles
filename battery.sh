dd8d572ad26134c36b9c2f84d0bf30d15de5f8d6
Divyansh2512 <2512sharmadivyansh@gmail.com>
2021-05-10 00:58:28 +0530

    Remove obsolete files

--- battery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BATTERYPATH=/sys/class/power_supply/BAT0/capacity
-if [ -f $BATTERYPATH ]; then
-    cat $BATTERYPATH
-fi
--- pretty-uptime.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
-secs=$((upSeconds%60))
-mins=$((upSeconds/60%60))
-hours=$((upSeconds/3600%24))
-days=$((upSeconds/86400))
-UPTIME=$(printf "%dd%02dh%02dm" "$days" "$hours" "$mins")
-echo $UPTIME
