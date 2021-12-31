d49a29a2011fbe97e7c63cc417e47c90d04d5bfc
Ritiek Malhotra <ritiek.malhotra@wowlabz.com>
2022-01-01 00:43:54 +0530

    powerline: add disk segment as a dependency

--- setup.sh
+++ setup.sh
@@ -218,7 +218,8 @@ function install() {
         pip3 install psutil --user -U
         pip3 install git+https://github.com/ritiek/powerline.git --user -U
         pip3 install powerline-gitstatus --user -U
-        pip3 install git+https://github.com/mKaloer/powerline_mem_segment --user -U
+        pip3 install git+https://github.com/mKaloer/powerline_mem_segment.git --user -U
+        pip3 install git+https://github.com/mvrozanti/powerline-disk-segment.git --user -U
         pip3 install powerline-cpu-temp --user -U
         echo
     else
