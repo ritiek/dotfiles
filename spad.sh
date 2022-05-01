c22bdacebe358dc488d1655501b92e073e4e99ee
Ritiek Malhotra <ritiek.malhotra@wowlabz.com>
2022-05-01 23:41:16 +0530

    Fetch touchscreen ID by device name

--- spad.sh
+++ spad.sh
@@ -6,12 +6,12 @@ if [ $1 -lt 1 ]; then
   # Switch off display
   xrandr --output HDMI-2 --off
   # Recalibrate touchscreen
-  xinput map-to-output 12 eDP-1
+  xinput map-to-output $(xinput list --id-only 'ELAN9009:00 04F3:2E4E') eDP-1
 else
   # Correct touchpad display orientation
   xrandr --auto --output HDMI-2 --mode 1080x2160 --pos 0x1080; xrandr --output HDMI-2 --rotate right
   # Recalibrate touchscreen
-  xinput map-to-output 12 eDP-1
+  xinput map-to-output $(xinput list --id-only 'ELAN9009:00 04F3:2E4E') eDP-1
   # Set touchpad brightness
   echo $1 | sudo tee /sys/class/leds/asus::screenpad/brightness
 fi
