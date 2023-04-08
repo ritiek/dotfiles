3569565f5d9e0859d6cf84145ad5f133c02aa5ba
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2023-04-08 19:28:15 +0530

    Install pynvim from arch repo

--- setup.sh
+++ setup.sh
@@ -396,7 +396,11 @@ function install() {
 
     # NVim configuration
     echo "Installing NeoVim configuration"
-    pip3 install pynvim
+    if [[ $is_arch == true ]]; then
+        sudo pacman --noconfirm -S python-pynvim
+    else
+        pip3 install pynvim
+    fi
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     mkdir -p ~/.config/nvim
