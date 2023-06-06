66254ada3406e57b5598d636ca3460fe8f2347de
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2023-06-06 12:38:51 +0530

    Sync up

--- chezmoi/dot_config/nvim/lua/custom/init.lua
+++ chezmoi/dot_config/nvim/lua/custom/init.lua
@@ -1,4 +1,8 @@
+local opt = vim.opt
 local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto reload files
+opt.autoread = true
 
 -- Auto resize panes when resizing nvim window
 autocmd("VimResized", {
