8c07439635b40864245cea07538be5858371708f
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-02-02 23:36:39 +0530

    Post battle testing upgrades

--- chezmoi/.chezmoiscripts/run_once_after_20-enable-systemd-services.sh
+++ chezmoi/.chezmoiscripts/run_once_after_20-enable-systemd-services.sh
@@ -3,6 +3,10 @@
 sudo systemctl enable --now tailscaled
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now swayosd-libinput-backend.service
+sudo systemctl enable --now auto-cpufreq
+# Enables hostname.local domains on local network, making stuff like below possible:
+# ssh pi@raspberrypi.local
+sudo systemctl enable --now avahi-daemon
 
 sudo systemctl enable nvidia-suspend.service
 sudo systemctl enable nvidia-hibernate.service
