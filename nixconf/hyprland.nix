7dff31942c5184a0e2ce5c36d49bf169585611b4
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-04-30 05:31:04 +0530

    swaync config, set wallpaper

--- nixconf/hyprland.nix
+++ nixconf/hyprland.nix
@@ -1,14 +1,17 @@
 { pkgs, config, ... }:
 {
   home.file.hyprland = {
-    source = config.lib.file.mkOutOfStoreSymlink ./chezmoi/dot_config/hypr/hyprland;
+    source = config.lib.file.mkOutOfStoreSymlink ../chezmoi/dot_config/hypr/hyprland;
     target = "${config.home.homeDirectory}/.config/hypr/hyprland";
   };
   wayland.windowManager.hyprland = {
     enable = true;
     enableNvidiaPatches = true;
     # xwayland.enable = false;
-    systemd.enable = true;
+    systemd = {
+      enable = true;
+      variables = ["-all"];
+    };
     extraConfig = builtins.readFile /etc/nixos/chezmoi/dot_config/hypr/hyprland.conf;
   };
 }
