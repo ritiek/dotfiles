d97b242c9fe5cdbf75eb11e6f033ad7179601ab8
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-05-01 18:57:43 +0530

    Install hyprlock through module

--- nixconf/hyprlock.nix
+++ nixconf/hyprlock.nix
@@ -1,8 +1,8 @@
 { pkgs, config, ... }:
 {
-  # home.packages = with pkgs; [
-  #   unstable.hyprlock
-  # ];
+  home.packages = with pkgs; [
+    unstable.hyprlock
+  ];
   home.file.hyprlock = {
     source = config.lib.file.mkOutOfStoreSymlink ../chezmoi/dot_config/hypr/hyprlock.conf;
     target = "${config.home.homeDirectory}/.config/hypr/hyprlock.conf";
