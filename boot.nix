a70a2b425820a84ed001da31e94c534d22ee78e3
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-06-06 01:19:29 +0530

    An attempt at RPi4

---
+++ boot.nix
@@ -0,0 +1,9 @@
+{
+  # Force Wayland on all apps.
+  # environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.kernelModules = [ "kvm-intel" "vhci-hcd" ];
+}
