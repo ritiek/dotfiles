6f21cf24b130b08f3ac5b6894bdfb685b92fc081
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-06-07 21:22:10 +0530

    Fine tune RPi4 a bit

--- rpi4.nix
+++ rpi4.nix
@@ -1,24 +1,23 @@
 { config, pkgs, lib, ... }:
+let
+  nixos-hardware = builtins.fetchTarball "https://github.com/NixOS/nixos-hardware/archive/master.tar.gz";
+in
 {
   imports = [
-    ["${builtins.fetchTarball "https://github.com/NixOS/nixos-hardware/archive/${nixosHardwareVersion}.tar.gz" }/raspberry-pi/4"];
+    (import "${nixos-hardware}/raspberry-pi/4")
   ];
 
   services.openssh.enable = true;
   hardware = {
-    pulseaudio.enable = true;
-    raspberry-pi."4".apply-overlays-dtmerge.enable = true;
-    raspberry-pi."4".fkms-3d.enable = true;
-    raspberry-pi."4".audio.enable = true;
-    deviceTree = {
-      enable = true;
-      filter = "*rpi-4-*.dtb";
-    };
+    # pulseaudio.enable = true;
+    # raspberry-pi."4".apply-overlays-dtmerge.enable = true;
+    # raspberry-pi."4".fkms-3d.enable = true;
+    # raspberry-pi."4".audio.enable = true;
   };
   console.enable = false;
   environment.systemPackages = with pkgs; [
     libraspberrypi
     raspberrypi-eeprom
   ];
-  system.stateVersion = "23.11";
+  # system.stateVersion = "23.11";
 }
