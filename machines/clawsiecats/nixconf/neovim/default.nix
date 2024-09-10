5292a05f244bd36d3868f6c3752dfcf5108b7641
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-09-10 15:18:55 +0530

    Add typescript LSP back in

--- machines/clawsiecats/nixconf/neovim/default.nix
+++ machines/clawsiecats/nixconf/neovim/default.nix
@@ -5,9 +5,6 @@
     enable = true;
     defaultEditor = true;
 
-    # colorschemes.gruvbox.enable = true;
-    # plugins.lightline.enable = true;
-
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
@@ -17,7 +14,6 @@
 
     extraPackages = with pkgs; [
       xclip
-      wl-clipboard-rs
       stylua
       deno
       clang-tools
@@ -25,21 +21,13 @@
       python312
       python312Packages.python-lsp-server
       rust-analyzer
-      # typescript-language-server
+      typescript-language-server
       gcc
       cargo
       rustfmt
     ];
-
-    # plugins = with pkgs.vimPlugins; [
-    # ];
   };
 
-  # xdg.configFile = {
-  #   nvim = {
-  #     source =  "/etc/nixos/chezmoi/dot_config/nvim/";
-  #   };
-  # };
   home.file.nvim = {
     source =  ./nvim;
     target = "${config.home.homeDirectory}/.config/nvim";
