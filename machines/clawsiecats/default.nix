95839a18730679ad8251cc584b85c2a9e082ffab
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-09-10 19:05:32 +0530

    Bleed a lil edge on clawspie kernel

--- machines/clawsiecats/default.nix
+++ machines/clawsiecats/default.nix
@@ -7,6 +7,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "clawsiecats";
 
