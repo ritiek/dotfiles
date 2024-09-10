b4da1a0cb6183e9570a26c5f12c94cf143410f50
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-09-10 14:44:59 +0530

    Trim clawsiecats

--- machines/clawsiecats/default.nix
+++ machines/clawsiecats/default.nix
@@ -1,29 +1,25 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "clawsiecats"; # Define your hostname.
+  networking.hostName = "clawsiecats";
+
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # time.timeZone = "Asia/Kolkata";
+  time.timeZone = "Asia/Kolkata";
   nix.settings = {
     experimental-features = [ "nix-command" " flakes" ];
     auto-optimise-store = true;
   };
+
   users = {
     defaultUserShell = pkgs.zsh;
     users.root.openssh.authorizedKeys.keys = [
@@ -38,42 +34,9 @@
   #   git
   # ];
 
-  programs.zsh.enable = true;
-  # programs = {
-  #   neovim = {
-  #     enable = true;
-  #     defaultEditor = true;
-  #     configure.customRC = ''
-  #       set number
-  #       set undofile
-  #     '';
-  #   };
-  #   zsh = {
-  #     enable = true;
-  #     autosuggestions.enable = true;
-  #     # syntaxHighlighting.enable = true;
-  #     ohMyZsh = {
-  #       enable = true;
-  #       theme = "alanpeabody";
-  #       plugins = [
-  #         "command-not-found"
-  #       ];
-  #     };
-  #     syntaxHighlighting = {
-  #       enable = true;
-  #       styles = {
-  #         path = "fg=cyan";
-  #         path_prefix = "fg=magenta";
-  #       };
-  #       highlighters = [
-  #         "main"
-  #         "brackets"
-  #       ];
-  #     };
-  #   };
-  #   command-not-found.enable = true;
-  # };
-
+  programs = {
+    zsh.enable = true;
+  };
   services = {
     openssh.enable = true;
     tailscale.enable = true;
@@ -88,11 +51,6 @@
 
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
@@ -112,6 +70,4 @@
   #
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
-
