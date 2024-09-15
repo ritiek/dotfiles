14764c7a0092b56493b84b50ea2dee9ac16700b8
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-09-15 22:19:20 +0530

    Neovim config impermanence

--- machines/clawsiecats/default.nix
+++ machines/clawsiecats/default.nix
@@ -103,6 +103,11 @@
   powerManagement.cpuFreqGovernor = "performance";
   zramSwap.enable = true;
 
+  boot.tmp = {
+    useTmpfs = true;
+    cleanOnBoot = true;
+  };
+
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
   #
--- machines/clawsiecats/home/default.nix
+++ machines/clawsiecats/home/default.nix
@@ -12,6 +12,7 @@
     persistence."/nix/persist/${config.home.homeDirectory}" = {
       files = [
         ".zsh_history"
+        ".local/share/nvim"
       ];
     };
     stateVersion = "24.05";
