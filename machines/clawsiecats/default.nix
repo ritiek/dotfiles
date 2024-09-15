aac2a167d6dd92dd91b8bc21de068d9b7d278146
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-09-15 22:41:36 +0530

    Final tweaks

--- machines/clawsiecats/default.nix
+++ machines/clawsiecats/default.nix
@@ -81,6 +81,9 @@
     openssh = {
       enable = true;
       openFirewall = true;
+      knownHosts = {
+        "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+      };
     };
     tailscale = {
       enable = true;
