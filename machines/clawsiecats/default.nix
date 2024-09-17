94cbeefab243cb275d73eae0c4fe4d10c9bf5c17
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-09-17 20:07:32 +0530

    Oops

--- machines/clawsiecats/default.nix
+++ machines/clawsiecats/default.nix
@@ -46,7 +46,6 @@
       # settings = {
       #   PasswordAuthentication = false;
       # };
-      openFirewall = true;
       knownHosts = {
         "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
       };
