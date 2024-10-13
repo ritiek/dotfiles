723639fc4ecd7475b8939817f96b780439064303
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-10-13 06:20:08 +0530

    Sync up with NvChad upstream

--- chezmoi/dot_config/nvim/init.lua
+++ chezmoi/dot_config/nvim/init.lua
@@ -1,5 +1,7 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
+-- show line numbers
+vim.wo.number = true
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
