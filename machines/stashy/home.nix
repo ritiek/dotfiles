4fa089fbdfe36ea7092fe7e60cce2ce3355fc414
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-10-24 19:24:35 +0530

    Extend stashy from nixypi

---
+++ machines/stashy/home.nix
@@ -0,0 +1,59 @@
+{ pkgs, inputs, config, ... }:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = {
+      inherit inputs;
+    };
+  };
+
+  environment.pathsToLink = [
+    "/share/zsh"
+    "/share/xdg-desktop-portal"
+    "/share/applications"
+  ];
+
+  home-manager.users.root = {
+    imports = [
+      ./../../home/zsh
+      ./../../home/git
+      ./../../home/neovim
+      ./../../home/zellij.nix
+      ./../../home/btop.nix
+    ];
+    home = {
+      stateVersion = "24.11";
+      packages = with pkgs; [
+        any-nix-shell
+
+        unzip
+        unrar-wrapper
+        sd
+        diskonaut
+        compsize
+
+        iptables
+        nmap
+        dig
+        cryptsetup
+        openssl
+        sops
+
+        miniserve
+        bore-cli
+      ];
+    };
+    programs = {
+      command-not-found.enable = true;
+      home-manager.enable = true;
+      jq.enable = true;
+      ripgrep.enable = true;
+      fd.enable = true;
+    };
+  };
+}
