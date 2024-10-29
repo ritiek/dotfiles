32793d643e4df0ece3bf46da3be72b874fa2b262
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-10-29 13:52:55 +0530

    Craft a wifite alias

--- machines/nutcracker/home.nix
+++ machines/nutcracker/home.nix
@@ -32,5 +32,27 @@
         wifite2
       ];
     };
+    programs.zsh.shellAliases = {
+      # TODO: We should auto-start this in a Zellij session. And maybe link some kind of
+      # message broker or IM for PNs related to updates.
+      wifite-preconfig = ''
+        wifite \
+          -i $(ls /sys/class/net | grep -vE '^(lo|tailscale0|wlan0)$' | head -n 1) \
+          -E $(iwgetid -r) \
+          --wpa \
+          --no-wps \
+          --skip-crack \
+          --no-pmkid \
+          --showb \
+          --allbands \
+          --pillage 120 \
+          --wpat 99999 \
+          --wpadt 90 \
+          --num-deauths 10 \
+          --hs-dir handshakes
+
+          # --clients-only
+        '';
+    };
   };
 }
