fbeb7e2347dd0b4fde5be990d864073a5ec8bca9
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-01 20:30:25 +0530

    Add a new machine pilab!

---
+++ machines/pilab/home/default.nix
@@ -0,0 +1,61 @@
+{ pkgs, inputs, config, ... }:
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = {
+      inherit inputs;
+    };
+  };
+
+  environment.pathsToLink = [
+    "/share/zsh"
+    "/share/xdg-desktop-portal"
+    "/share/applications"
+  ];
+
+  home-manager.users.root = {
+    imports = [
+      # ./../../../home/gnupg.nix
+      ./../../../modules/home/zsh
+      ./../../../modules/home/git
+      ./../../../modules/home/neovim
+      ./../../../modules/home/zellij.nix
+      ./../../../modules/home/btop.nix
+    ];
+    home = {
+      stateVersion = "24.11";
+      packages = with pkgs; [
+        any-nix-shell
+
+        unzip
+        unrar-wrapper
+        sd
+        diskonaut
+        compsize
+
+        iptables
+        nmap
+        dig
+        cryptsetup
+        openssl
+        sops
+        deploy-rs
+
+        miniserve
+        bore-cli
+      ];
+    };
+    programs = {
+      command-not-found.enable = true;
+      home-manager.enable = true;
+      jq.enable = true;
+      ripgrep.enable = true;
+      fd.enable = true;
+    };
+  };
+}
---
+++ machines/pilab/hw-config.nix
@@ -0,0 +1,32 @@
+{ inputs, ... }:
+{
+  imports = [
+    inputs.raspberry-pi-nix.nixosModules.raspberry-pi
+  ];
+
+  raspberry-pi-nix.board = "bcm2712";
+  hardware = {
+    raspberry-pi = {
+      config = {
+        all = {
+          base-dt-params = {
+            BOOT_UART = {
+              value = 1;
+              enable = true;
+            };
+            uart_2ndstage = {
+              value = 1;
+              enable = true;
+            };
+          };
+          dt-overlays = {
+            disable-bt = {
+              enable = true;
+              params = { };
+            };
+          };
+        };
+      };
+    };
+  };
+}
