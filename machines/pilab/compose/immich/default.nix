f6ce414bfebffebedef172d1d325dbaad38a2c40
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-06 00:23:46 +0530

    Switch to docker from podman
    Running a stack which has multiple containers that share the network
    interface between themselves seems more complex using podman when
    compared to podman.

--- machines/pilab/compose/immich/default.nix
+++ machines/pilab/compose/immich/default.nix
@@ -8,20 +8,18 @@
   };
 
   # Runtime
-  virtualisation.podman = {
+  virtualisation.docker = {
     enable = true;
     autoPrune.enable = true;
-    dockerCompat = true;
-    defaultNetwork.settings = {
-      # Required for container networking to be able to use names.
-      dns_enabled = true;
-    };
   };
-  virtualisation.oci-containers.backend = "podman";
+  virtualisation.oci-containers.backend = "docker";
 
   # Containers
   virtualisation.oci-containers.containers."immich_machine_learning" = {
     image = "ghcr.io/immich-app/immich-machine-learning:release";
+    environment = {
+      "TZ" = "Asia/Kolkata";
+    };
     environmentFiles = [
       config.sops.secrets."env.immich".path
     ];
@@ -29,30 +27,34 @@
       "/media/services/immich/model-cache:/cache:rw"
     ];
     log-driver = "journald";
+    autoStart = false;
     extraOptions = [
       "--network-alias=immich-machine-learning"
       "--network=immich_default"
     ];
   };
-  systemd.services."podman-immich_machine_learning" = {
+  systemd.services."docker-immich_machine_learning" = {
     serviceConfig = {
       Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
     };
     after = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
     requires = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
-    partOf = [
-      "podman-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-immich-root.target"
+    unitConfig.RequiresMountsFor = [
+      "/media/services/immich/model-cache"
     ];
   };
   virtualisation.oci-containers.containers."immich_postgres" = {
     image = "tensorchord/pgvecto-rs:pg14-v0.2.0@sha256:90724186f0a3517cf6914295b5ab410db9ce23190a2d9d0b9dd6463e3fa298f0";
+    environment = {
+      "TZ" = "Asia/Kolkata";
+    };
     environmentFiles = [
       config.sops.secrets."env.immich".path
     ];
@@ -60,26 +62,27 @@
       "/media/services/immich/pgdata:/var/lib/postgresql/data:rw"
     ];
     log-driver = "journald";
+    autoStart = false;
     extraOptions = [
       "--network-alias=database"
       "--network=immich_default"
     ];
   };
-  systemd.services."podman-immich_postgres" = {
+  systemd.services."docker-immich_postgres" = {
     serviceConfig = {
       Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
     };
     after = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
     requires = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
-    partOf = [
-      "podman-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-immich-root.target"
+    unitConfig.RequiresMountsFor = [
+      "/media/services/immich/pgdata"
     ];
   };
   virtualisation.oci-containers.containers."immich_redis" = {
@@ -88,30 +91,31 @@
       config.sops.secrets."env.immich".path
     ];
     log-driver = "journald";
+    autoStart = false;
     extraOptions = [
       "--network-alias=redis"
       "--network=immich_default"
     ];
   };
-  systemd.services."podman-immich_redis" = {
+  systemd.services."docker-immich_redis" = {
     serviceConfig = {
       Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
     };
     after = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
     requires = [
-      "podman-network-immich_default.service"
-    ];
-    partOf = [
-      "podman-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-immich-root.target"
+      "docker-network-immich_default.service"
     ];
   };
   virtualisation.oci-containers.containers."immich_server" = {
     image = "ghcr.io/immich-app/immich-server:release";
+    environment = {
+      "TZ" = "Asia/Kolkata";
+    };
     environmentFiles = [
       config.sops.secrets."env.immich".path
     ];
@@ -127,51 +131,52 @@
       "immich_redis"
     ];
     log-driver = "journald";
+    autoStart = false;
     extraOptions = [
       "--network-alias=immich-server"
       "--network=immich_default"
     ];
   };
-  systemd.services."podman-immich_server" = {
+  systemd.services."docker-immich_server" = {
     serviceConfig = {
       Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
     };
     after = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
     requires = [
-      "podman-network-immich_default.service"
+      "docker-network-immich_default.service"
     ];
-    partOf = [
-      "podman-compose-immich-root.target"
-    ];
-    wantedBy = [
-      "podman-compose-immich-root.target"
+    unitConfig.RequiresMountsFor = [
+      "/etc/localtime"
+      "/media/services/immich/photos"
     ];
   };
 
   # Networks
-  systemd.services."podman-network-immich_default" = {
-    path = [ pkgs.podman ];
+  systemd.services."docker-network-immich_default" = {
+    path = [ pkgs.docker ];
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
-      ExecStop = "podman network rm -f immich_default";
+      ExecStop = "docker network rm -f immich_default";
     };
     script = ''
-      podman network inspect immich_default || podman network create immich_default
+      docker network inspect immich_default || docker network create immich_default
     '';
-    partOf = [ "podman-compose-immich-root.target" ];
-    wantedBy = [ "podman-compose-immich-root.target" ];
+    partOf = [ "docker-compose-immich-root.target" ];
+    wantedBy = [ "docker-compose-immich-root.target" ];
   };
 
   # Root service
   # When started, this will automatically create all resources and start
   # the containers. When stopped, this will teardown all resources.
-  systemd.targets."podman-compose-immich-root" = {
+  systemd.targets."docker-compose-immich-root" = {
     unitConfig = {
       Description = "Root target generated by compose2nix.";
     };
-    wantedBy = [ "multi-user.target" ];
   };
 }
---
+++ machines/pilab/compose/tubearchivist/default.nix
@@ -0,0 +1,170 @@
+# Auto-generated using compose2nix v0.2.3.
+{ pkgs, lib, config, ... }:
+
+{
+  sops.secrets."env.tubearchivist" = {
+    sopsFile = ./stack.env;
+    format = "dotenv";
+  };
+
+  # Runtime
+  virtualisation.docker = {
+    enable = true;
+    autoPrune.enable = true;
+  };
+  virtualisation.oci-containers.backend = "docker";
+
+  # Containers
+  virtualisation.oci-containers.containers."archivist-es" = {
+    image = "docker.elastic.co/elasticsearch/elasticsearch:8.9.0";
+    environmentFiles = [
+      config.sops.secrets."env.tubearchivist".path
+    ];
+    volumes = [
+      "/media/services/tubearchivist/es:/usr/share/elasticsearch/data:rw"
+    ];
+    log-driver = "journald";
+    autoStart = false;
+    extraOptions = [
+      "--network-alias=archivist-es"
+      "--network=tubearchivist_default"
+    ];
+  };
+  systemd.services."docker-archivist-es" = {
+    serviceConfig = {
+      Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
+    };
+    after = [
+      "docker-network-tubearchivist_default.service"
+    ];
+    requires = [
+      "docker-network-tubearchivist_default.service"
+    ];
+    unitConfig.RequiresMountsFor = [
+      "/media/services/tubearchivist/es"
+    ];
+  };
+  virtualisation.oci-containers.containers."archivist-redis" = {
+    image = "redis/redis-stack-server";
+    environmentFiles = [
+      config.sops.secrets."env.tubearchivist".path
+    ];
+    volumes = [
+      "tubearchivist_redis:/data:rw"
+    ];
+    dependsOn = [
+      "archivist-es"
+    ];
+    log-driver = "journald";
+    autoStart = false;
+    extraOptions = [
+      "--network-alias=archivist-redis"
+      "--network=tubearchivist_default"
+    ];
+  };
+  systemd.services."docker-archivist-redis" = {
+    serviceConfig = {
+      Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
+    };
+    after = [
+      "docker-network-tubearchivist_default.service"
+      "docker-volume-tubearchivist_redis.service"
+    ];
+    requires = [
+      "docker-network-tubearchivist_default.service"
+      "docker-volume-tubearchivist_redis.service"
+    ];
+  };
+  virtualisation.oci-containers.containers."tubearchivist" = {
+    image = "bbilly1/tubearchivist";
+    environmentFiles = [
+      config.sops.secrets."env.tubearchivist".path
+    ];
+    volumes = [
+      "/media/services/tubearchivist/cache:/cache:rw"
+      "/media/services/tubearchivist/videos:/youtube:rw"
+    ];
+    ports = [
+      "8454:8000/tcp"
+    ];
+    dependsOn = [
+      "archivist-es"
+      "archivist-redis"
+    ];
+    log-driver = "journald";
+    autoStart = false;
+    extraOptions = [
+      "--add-host=host.docker.internal:host-gateway"
+      "--health-cmd=[\"curl\",\"-f\",\"http://localhost:8000/health\"]"
+      "--health-interval=2m0s"
+      "--health-retries=3"
+      "--health-start-period=30s"
+      "--health-timeout=10s"
+      "--network-alias=tubearchivist"
+      "--network=tubearchivist_default"
+    ];
+  };
+  systemd.services."docker-tubearchivist" = {
+    serviceConfig = {
+      Restart = lib.mkOverride 500 "always";
+      RestartMaxDelaySec = lib.mkOverride 500 "1m";
+      RestartSec = lib.mkOverride 500 "100ms";
+      RestartSteps = lib.mkOverride 500 9;
+    };
+    after = [
+      "docker-network-tubearchivist_default.service"
+    ];
+    requires = [
+      "docker-network-tubearchivist_default.service"
+    ];
+    unitConfig.RequiresMountsFor = [
+      "/media/services/tubearchivist/cache"
+      "/media/services/tubearchivist/videos"
+    ];
+  };
+
+  # Networks
+  systemd.services."docker-network-tubearchivist_default" = {
+    path = [ pkgs.docker ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStop = "docker network rm -f tubearchivist_default";
+    };
+    script = ''
+      docker network inspect tubearchivist_default || docker network create tubearchivist_default
+    '';
+    partOf = [ "docker-compose-tubearchivist-root.target" ];
+    wantedBy = [ "docker-compose-tubearchivist-root.target" ];
+  };
+
+  # Volumes
+  systemd.services."docker-volume-tubearchivist_redis" = {
+    path = [ pkgs.docker ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStop = "docker volume rm -f tubearchivist_redis";
+    };
+    script = ''
+      docker volume inspect tubearchivist_redis || docker volume create tubearchivist_redis
+    '';
+    partOf = [ "docker-compose-tubearchivist-root.target" ];
+    wantedBy = [ "docker-compose-tubearchivist-root.target" ];
+  };
+
+  # Root service
+  # When started, this will automatically create all resources and start
+  # the containers. When stopped, this will teardown all resources.
+  systemd.targets."docker-compose-tubearchivist-root" = {
+    unitConfig = {
+      Description = "Root target generated by compose2nix.";
+    };
+  };
+}
