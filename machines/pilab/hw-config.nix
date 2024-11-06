00912d6d20e40edf390d80efc47e07bd9fe8b474
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-07 02:57:59 +0530

    Use DHCP with ethernet

--- machines/pilab/hw-config.nix
+++ machines/pilab/hw-config.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [
     inputs.raspberry-pi-nix.nixosModules.raspberry-pi
@@ -29,4 +29,13 @@
       };
     };
   };
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
 }
