c7ae9a3217f166c5aa8feabbcc8e07fcc9b31417
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-18 00:28:37 +0530

    uptime-kuma isn't being used at the moment

--- machines/stashy/services.nix
+++ machines/stashy/services.nix
@@ -2,15 +2,15 @@
 
 {
   services = {
-    uptime-kuma = {
-      enable = true;
-      appriseSupport = false;
-      settings = {
-        HOST = "0.0.0.0";
-        PORT = "3001";
-        # FIXME: This results in a permission error during nixos-rebuild.
-        # DATA_DIR = lib.mkForce "/root/uptime-kuma";
-      };
-    };
+    # uptime-kuma = {
+    #   enable = true;
+    #   appriseSupport = false;
+    #   settings = {
+    #     HOST = "0.0.0.0";
+    #     PORT = "3001";
+    #     # FIXME: This results in a permission error during nixos-rebuild.
+    #     # DATA_DIR = lib.mkForce "/root/uptime-kuma";
+    #   };
+    # };
   };
 }
