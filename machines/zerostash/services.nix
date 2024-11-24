fb5aedde3ec7f2741e5a3b7f5004913031448a98
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-24 23:56:44 +0530

    Set restic dataDir through fileSystems config

--- machines/zerostash/services.nix
+++ machines/zerostash/services.nix
@@ -3,15 +3,16 @@
 {
   sops.secrets."restic.htpasswd".owner = "restic";
 
+  # The following don't seem to be needed for auto-mount when the
+  # "x-systemd.automount" filesystem mount option is set.
   # services.devmon.enable = true;
   # services.gvfs.enable = true; 
   # services.udisks2.enable = true;
 
   services.restic.server = {
-    # FALSEEEEEEEEEEEEEEEEEEEEE
     enable = true;
     listenAddress = "0.0.0.0:52525";
-    dataDir = "/media";
+    dataDir = config.fileSystems.restic-backup.mountPoint;
     # privateRepos = true;
     extraFlags = [
       "--htpasswd-file=${config.sops.secrets."restic.htpasswd".path}"
