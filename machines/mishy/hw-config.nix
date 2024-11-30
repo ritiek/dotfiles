0507e5ef7bea914c6a2bfc51aa97452217dd3d94
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2024-11-30 22:21:10 +0530

    Mount nix store with noatime

--- machines/mishy/hw-config.nix
+++ machines/mishy/hw-config.nix
@@ -16,6 +16,7 @@
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/69b73231-f1da-4668-9c5a-06bf9592fd80";
       fsType = "ext4";
+      options = [ "noatime" ];
     };
 
   fileSystems."/boot" =
