13f18502c5584ec865f765658dcbc8d205dcc36d
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-01-26 10:39:40 +0530

    Offload firefox to GPU

--- machines/mishy/graphics.nix
+++ machines/mishy/graphics.nix
@@ -16,7 +16,7 @@
   };
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
+  services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
 
   hardware.nvidia = {
     # Modesetting is required.
