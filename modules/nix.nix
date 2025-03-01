c76e5e176df17098b16108e4ce07608b123ebcc5
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-03-01 21:01:14 +0530

    Specify build cache for rockchip computers

--- modules/nix.nix
+++ modules/nix.nix
@@ -9,6 +9,7 @@
       "https://hyprland.cachix.org"
       "https://cache.nixos.org"
       "https://nixpkgs-wayland.cachix.org"
+      "https://nabam-nixos-rockchip.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
@@ -16,6 +17,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM="
     ];
     trusted-users = [ "ritiek" ];
   };
