1f2f2a8df68b99670049f24301d65c26f09c425e
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-03-08 22:30:38 +0530

    A failed attempt to fix wifi

--- machines/radrubble/hw-config.nix
+++ machines/radrubble/hw-config.nix
@@ -15,6 +15,10 @@ in
     inputs.rockchip.nixosModules.noZFS
   ];
 
+  # pkgs.armbian-firmware is non-free.
+  nixpkgs.config.allowUnfree = true;
+  hardware.firmware = with pkgs; [ armbian-firmware ];
+
   rockchip.uBoot = inputs.rockchip.packages."aarch64-linux".uBootRadxaCM3IO;
   boot.kernelPackages = inputs.rockchip.legacyPackages."aarch64-linux".kernel_linux_6_12_rockchip;
 }
