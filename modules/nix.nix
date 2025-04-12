a7727484909764406421541f78bc106907d9fb76
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-04-12 21:10:14 +0530

    Query binary cache from numtide

--- modules/nix.nix
+++ modules/nix.nix
@@ -5,19 +5,21 @@
     auto-optimise-store = true;
     substituters = [
       "https://nix-community.cachix.org"
+      "https://numtide.cachix.org"
       "https://cache.garnix.io"
-      "https://hyprland.cachix.org"
       "https://cache.nixos.org"
       "https://nixpkgs-wayland.cachix.org"
       "https://nabam-nixos-rockchip.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
     trusted-users = [ "ritiek" ];
   };
