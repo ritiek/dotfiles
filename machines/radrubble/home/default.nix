329c227f10658b04b6567188b5ec99451f1bc8d4
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-04-27 11:28:49 +0530

    Attempt at multi-user machines

--- machines/radrubble/home/default.nix
+++ machines/radrubble/home/default.nix
@@ -1,7 +1,7 @@
 { pkgs, inputs, config, ... }:
 {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
+    ./ritiek
   ];
 
   home-manager = {
@@ -24,53 +24,5 @@
       ./../../../modules/home/neovim
     ];
     home.stateVersion = "24.11";
-  };
-
-  systemd.tmpfiles.settings."10-ssh"."/home/ritiek/.ssh/sops.id_ed25519" = {
-    "C+" = {
-      mode = "0600";
-      user = "ritiek";
-      argument = "/etc/ssh/ssh_host_ed25519_key";
-    };
-  };
-  home-manager.users.ritiek = {
-    imports = [
-      # ./../../../home/gnupg.nix
-      ./../../../modules/home/zsh
-      ./../../../modules/home/git
-      ./../../../modules/home/neovim
-      ./../../../modules/home/zellij.nix
-      ./../../../modules/home/btop.nix
-    ];
-    home = {
-      stateVersion = "24.11";
-      packages = with pkgs; [
-        any-nix-shell
-
-        unzip
-        unrar-wrapper
-        sd
-        # diskonaut
-        compsize
-
-        iptables
-        nmap
-        dig
-        cryptsetup
-        openssl
-        sops
-        deploy-rs
-
-        miniserve
-        bore-cli
-      ];
-    };
-    programs = {
-      command-not-found.enable = true;
-      home-manager.enable = true;
-      jq.enable = true;
-      ripgrep.enable = true;
-      fd.enable = true;
-    };
   };
 }
