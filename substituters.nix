bee5aa547657ffb3d9462f94660a5de234cc0439
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-08-17 04:38:49 +0530

    Define shared substituters

---
+++ substituters.nix
@@ -0,0 +1,29 @@
+{
+  substituters = [
+    "https://cache.nixos.org"
+    "https://nix-community.cachix.org"
+    "https://numtide.cachix.org"
+    "https://cache.garnix.io"
+    "https://nixos-raspberrypi.cachix.org"
+    "https://nixpkgs-wayland.cachix.org"
+    # "https://nabam-nixos-rockchip.cachix.org"
+    "https://hyprland.cachix.org"
+
+    "http://pilab.lion-zebra.ts.net:7080/attic-action"
+    # In case my server is not reachable over Tailscale.
+    "https://attic.clawsiecats.omg.lol/attic-action"
+  ];
+
+  trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+    "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    # "nabam-nixos-rockchip.cachix.org-1:BQDltcnV8GS/G86tdvjLwLFz1WeFqSk7O9yl+DR0AVM="
+    "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+
+    "attic-action:wjnBtx68f8TU7eahRKpVICGlIwOjQ7ENsCUiDd5Jqqs="
+  ];
+}
