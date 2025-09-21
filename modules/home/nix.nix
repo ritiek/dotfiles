f6a98d2a518bdaf548816002f13dd9ff048c073d
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-09-21 18:24:37 +0530

    Remove outdated GitHub token

--- modules/home/nix.nix
+++ modules/home/nix.nix
@@ -1,4 +1,6 @@
 { config, ... }:
 {
-  sops.secrets."nix.conf".path = "${config.home.homeDirectory}/.config/nix/nix.conf";
+  # TODO: Commenting this out for now as my GitHub token expired.
+  #       Generate a new one and uncomment this.
+  # sops.secrets."nix.conf".path = "${config.home.homeDirectory}/.config/nix/nix.conf";
 }
