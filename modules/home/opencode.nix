c227c03ae36cd5a72f8c8942c0977f55e20e553d
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-10-19 22:53:37 +0530

    Modularize Opencode

---
+++ modules/home/opencode.nix
@@ -0,0 +1,17 @@
+{
+  programs.opencode = {
+    enable = true;
+    settings = {
+      model = "claude-sonnet-4-5-20250929";
+      theme = "cobalt2";
+      tui.scroll_speed = 5;
+      autoupdate = false;
+      # mcp.context7 = {
+      #   enabled = true;
+      #   type = "remote";
+      #   url = "https://mcp.context7.com/mcp";
+      #   headers.CONTEXT7_API_KEY = "INSERT_COIN";
+      # };
+    };
+  };
+}
