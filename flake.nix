1f387f1cc1d9f720c0e41e7cf8cae2a40f621ea8
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-10-19 06:46:25 +0530

    Add gotify-desktop service

--- flake.nix
+++ flake.nix
@@ -166,6 +166,12 @@
     };
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+
+    gotify-desktop = {
+      url = "github:ritiek/gotify-desktop/nix-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs = { self, ... }@inputs:
