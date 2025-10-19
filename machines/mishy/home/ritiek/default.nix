aa3af22177ef75101fdf8e65a3021403f333c7ab
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-10-19 22:05:47 +0530

    Play with opencode

--- machines/mishy/home/ritiek/default.nix
+++ machines/mishy/home/ritiek/default.nix
@@ -283,7 +283,21 @@
       };
     };
 
-    claude-code.enable = true;
+    opencode = {
+      enable = true;
+      settings = {
+        model = "claude-sonnet-4-5-20250929";
+        theme = "kanagawa";
+        tui.scroll_speed = 5;
+        autoupdate = false;
+        # mcp.context7 = {
+        #   enabled = true;
+        #   type = "remote";
+        #   url = "https://mcp.context7.com/mcp";
+        #   headers.CONTEXT7_API_KEY = "INSERT_COIN";
+        # };
+      };
+    };
   };
 
   services = {
