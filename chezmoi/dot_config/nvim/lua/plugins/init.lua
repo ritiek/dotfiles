a2c50f2f1f00fd769179d630edcec773f3d30fd1
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-10-20 20:31:56 +0530

    Make which-key.nvim and opencode.nvim accessible immediately

--- chezmoi/dot_config/nvim/lua/plugins/init.lua
+++ chezmoi/dot_config/nvim/lua/plugins/init.lua
@@ -1,4 +1,9 @@
 local plugins = {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+  },
+
   -- FIXME: Are we using this?
   -- {
   --   "stevearc/conform.nvim",
@@ -177,7 +182,7 @@ local plugins = {
 
   {
     "NickvanDyke/opencode.nvim",
-    event = "BufWinEnter",
+    event = "VeryLazy",
     dependencies = {
       -- Recommended for `ask()` and `select()`.
       -- Required for `toggle()`.
@@ -190,18 +195,6 @@ local plugins = {
 
       -- Required for `vim.g.opencode_opts.auto_reload`
       vim.opt.autoread = true
-
-      -- Recommended/example keymaps
-      vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask about this" })
-      vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end, { desc = "Select prompt" })
-      vim.keymap.set({ "n", "x" }, "<leader>o+", function() require("opencode").prompt("@this") end, { desc = "Add this" })
-      vim.keymap.set("n", "<leader>ot", function() require("opencode").toggle() end, { desc = "Toggle embedded" })
-      vim.keymap.set("n", "<leader>oc", function() require("opencode").command() end, { desc = "Select command" })
-      vim.keymap.set("n", "<leader>on", function() require("opencode").command("session_new") end, { desc = "New session" })
-      vim.keymap.set("n", "<leader>oi", function() require("opencode").command("session_interrupt") end, { desc = "Interrupt session" })
-      vim.keymap.set("n", "<leader>oA", function() require("opencode").command("agent_cycle") end, { desc = "Cycle selected agent" })
-      vim.keymap.set("n", "<S-C-u>",    function() require("opencode").command("messages_half_page_up") end, { desc = "Messages half page up" })
-      vim.keymap.set("n", "<S-C-d>",    function() require("opencode").command("messages_half_page_down") end, { desc = "Messages half page down" })
     end
   }
 
