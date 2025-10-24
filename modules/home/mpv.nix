5eb6ec580475c0ade58b27bac3a3a086c6b10320
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-10-25 02:22:24 +0530

    Fine tune mpv a bit

--- modules/home/mpv.nix
+++ modules/home/mpv.nix
@@ -17,13 +17,25 @@
       # vo = "gpu";
       # profile = "gpu-hq";
       # gpu-context = "wayland";
-      osc = "no";
+      osc = "yes";
     };
     scripts = with pkgs; [
       mpvScripts.mpv-cheatsheet
       mpvScripts.mpris
       mpvScripts.webtorrent-mpv-hook
-      mpvScripts.thumbnail
+      # XXX: Commenting for now cause this results in the following error:
+      # [file] Cannot open file '/nix/store/g7n76rhfna3ndpwjbpn5b7im9h5ff7li-mpv-thumbnail-script-0.5.4/share/mpv/scripts/mpv_thumbnail_script_{client_osc,server}.lua': No such file or directory
+      # Failed to open /nix/store/g7n76rhfna3ndpwjbpn5b7im9h5ff7li-mpv-thumbnail-script-0.5.4/share/mpv/scripts/mpv_thumbnail_script_{client_osc,server}.lua.
+      # [mpv_thumbnail_script__client_osc_server_]
+      # [mpv_thumbnail_script__client_osc_server_] stack traceback:
+      # [mpv_thumbnail_script__client_osc_server_]      [C]: in ?
+      # [mpv_thumbnail_script__client_osc_server_]      [C]: in ?
+      # [mpv_thumbnail_script__client_osc_server_] Lua error: Could not read file.
+      # [mpv_thumbnail_script__client_osc_server_]
+      # client removed during hook handling
+      #
+      # mpvScripts.thumbnail
+
       mpvScripts.thumbfast
       # mpvScripts.autosub  # Temporarily disabled due to subliminal build issues
       mpvScripts.mpv-notify-send
