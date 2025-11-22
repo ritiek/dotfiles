a42789c23bc6f94cb1babc722ff7cbef4eda116b
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-11-22 21:48:44 +0530

    Add X11 forwarding config to openssh

--- machines/pilab/default.nix
+++ machines/pilab/default.nix
@@ -176,6 +176,8 @@
       settings = {
         PermitRootLogin = "no";
         PasswordAuthentication = false;
+        X11Forwarding = true;
+        X11UseLocalhost = false;
       };
       knownHosts = {
         "github.com".publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
