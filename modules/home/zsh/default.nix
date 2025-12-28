3441238e9d3bf9df4f7243e4ffb78bae8040ed84
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-12-28 08:21:59 +0530

    Have nix figure out indentation automatically

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -3,8 +3,8 @@
   programs.zsh = {
     enable = true;
     profileExtra = ''
-export WAYLAND_DISPLAY=wayland-1
-# eval $(keychain --eval --quiet --noask)
+      export WAYLAND_DISPLAY=wayland-1
+      # eval $(keychain --eval --quiet --noask)
     '';
     initContent = let
       zshConfigBeforeCompInit = lib.mkOrder 550 ''
@@ -12,131 +12,131 @@ export WAYLAND_DISPLAY=wayland-1
         source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
       '';
       zshConfigEarlyInit = lib.mkOrder 500 ''
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "$HOME/.cache/p10k-instant-prompt-$USER.zsh" ]]; then
-  source "$HOME/.cache/p10k-instant-prompt-$USER.zsh"
-fi
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "$HOME/.cache/p10k-instant-prompt-$USER.zsh" ]]; then
+          source "$HOME/.cache/p10k-instant-prompt-$USER.zsh"
+        fi
 
-source $HOME/.p10k.zsh
-source $HOME/.zprofile
+        source $HOME/.p10k.zsh
+        source $HOME/.zprofile
 
-# Space prefix to suppress history
-setopt HIST_IGNORE_SPACE
+        # Space prefix to suppress history
+        setopt HIST_IGNORE_SPACE
 
-# Refresh commands-cache for tab-completion
-zstyle ":completion:*:commands" rehash 1
+        # Refresh commands-cache for tab-completion
+        zstyle ":completion:*:commands" rehash 1
 
-# Set your own notification threshold
-bgnotify_threshold=7
+        # Set your own notification threshold
+        bgnotify_threshold=7
 
-function bgnotify_formatted {
-  ## $1=exit_status, $2=command, $3=elapsed_time
-  [ $1 -eq 0 ] && title="Succeeded" || title="Failed"
-  bgnotify "$title in $3s" "$2";
-}
+        function bgnotify_formatted {
+          ## $1=exit_status, $2=command, $3=elapsed_time
+          [ $1 -eq 0 ] && title="Succeeded" || title="Failed"
+          bgnotify "$title in $3s" "$2";
+        }
 
-# Allow symlinks
-ZSH_DISABLE_COMPFIX=true
+        # Allow symlinks
+        ZSH_DISABLE_COMPFIX=true
 
-# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=59'
+        # ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=59'
 
-# Make Enter key on the numpad work as Return key
-bindkey '^[OM' accept-line
+        # Make Enter key on the numpad work as Return key
+        bindkey '^[OM' accept-line
 
-bindkey '^[[Z' reverse-menu-complete
-# Navigate to previous selection with shift+tab
-# when using tab completition for navigation
+        bindkey '^[[Z' reverse-menu-complete
+        # Navigate to previous selection with shift+tab
+        # when using tab completition for navigation
 
-# Enable Vi bindings
-bindkey -v
+        # Enable Vi bindings
+        bindkey -v
 
-# zsh-autosuggetsions maps
-## map autosuggest-accept to ctrl+/
-bindkey '^_' autosuggest-accept
-bindkey '^K' autosuggest-execute
+        # zsh-autosuggetsions maps
+        ## map autosuggest-accept to ctrl+/
+        bindkey '^_' autosuggest-accept
+        bindkey '^K' autosuggest-execute
 
-# Reverse search like in Bash
-# bindkey '^R' history-incremental-search-backward
-# bindkey '^S' history-incremental-search-forward
+        # Reverse search like in Bash
+        # bindkey '^R' history-incremental-search-backward
+        # bindkey '^S' history-incremental-search-forward
 
-# Commented out to allow fzf's fuzzy Ctrl+R history search
-# bindkey '^R' history-incremental-pattern-search-backward
-# bindkey '^S' history-incremental-pattern-search-forward
+        # Commented out to allow fzf's fuzzy Ctrl+R history search
+        # bindkey '^R' history-incremental-pattern-search-backward
+        # bindkey '^S' history-incremental-pattern-search-forward
 
-autoload edit-command-line
-zle -N edit-command-line
-bindkey -M vicmd ' ' edit-command-line
-bindkey "^?" backward-delete-char
+        autoload edit-command-line
+        zle -N edit-command-line
+        bindkey -M vicmd ' ' edit-command-line
+        bindkey "^?" backward-delete-char
 
-${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
-      '';
-    in lib.mkMerge [ zshConfigEarlyInit zshConfigBeforeCompInit ];
-      envExtra = ''
-# the default umask is set in /etc/profile; for setting the umask
-# for ssh logins, install and configure the libpam-umask package.
-#umask 022
+        ${pkgs.any-nix-shell}/bin/any-nix-shell zsh --info-right | source /dev/stdin
+              '';
+            in lib.mkMerge [ zshConfigEarlyInit zshConfigBeforeCompInit ];
+              envExtra = ''
+        # the default umask is set in /etc/profile; for setting the umask
+        # for ssh logins, install and configure the libpam-umask package.
+        #umask 022
 
-# export EDITOR="nvim"
-# This messes up Neovim config on NixOS, so commenting it out:
-# export VIM="$HOME/.config/nvim"
-# export VIMRUNTIME="/usr/share/nvim/runtime"
+        # export EDITOR="nvim"
+        # This messes up Neovim config on NixOS, so commenting it out:
+        # export VIM="$HOME/.config/nvim"
+        # export VIMRUNTIME="/usr/share/nvim/runtime"
 
-# Required by Opencode.
-export COLORTERM=truecolor
+        # Required by Opencode.
+        export COLORTERM=truecolor
 
-export BROWSER="zen-beta"
-export LESS="--mouse --wheel-lines=3 -r"
-export LESSOPEN="|$HOME/.lessfilter %s"
+        export BROWSER="zen-beta"
+        export LESS="--mouse --wheel-lines=3 -r"
+        export LESSOPEN="|$HOME/.lessfilter %s"
 
-# FZF options: hide progress counter
-export FZF_DEFAULT_OPTS='--info=hidden --height=30%'
-# Reduce lag when switching between Normal and Insert mode with Vi
-# bindings in zsh
-export KEYTIMEOUT=1
+        # FZF options: hide progress counter
+        export FZF_DEFAULT_OPTS='--info=hidden --height=30%'
+        # Reduce lag when switching between Normal and Insert mode with Vi
+        # bindings in zsh
+        export KEYTIMEOUT=1
 
-export OPENCV_LOG_LEVEL=ERROR
+        export OPENCV_LOG_LEVEL=ERROR
 
-export SDKMAN_DIR="$HOME/.sdkman"
-[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
+        export SDKMAN_DIR="$HOME/.sdkman"
+        [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# export JAVA_OPTS="-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee"
-export GOPATH="$HOME/go"
+        # export JAVA_OPTS="-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee"
+        export GOPATH="$HOME/go"
 
-# export POWERLINE_BASH_CONTINUATION=1
-# export POWERLINE_BASH_SELECT=1
+        # export POWERLINE_BASH_CONTINUATION=1
+        # export POWERLINE_BASH_SELECT=1
 
-export ESPIDF=/opt/esp-idf
+        export ESPIDF=/opt/esp-idf
 
-# export PYENV_ROOT="$HOME/.pyenv"
-# command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-# eval "$(pyenv init -)"
+        # export PYENV_ROOT="$HOME/.pyenv"
+        # command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+        # eval "$(pyenv init -)"
 
-# set PATH so it includes user's private bin directories
-PATH="$HOME/bin:$HOME/.local/bin:$PATH"
-PATH="$HOME/go/bin:$PATH"
-PATH="$HOME/.cabal/bin:$PATH"
-PATH="$HOME/.rbenv/bin:$PATH"
-PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
-PATH="$HOME/.rbenv/bin:$PATH"
-PATH="$HOME/bin:$PATH"
-PATH="$HOME/.local/bin:$PATH"
-PATH="$HOME/bin/gyb:$PATH"
-PATH="$HOME/.cargo/bin:$PATH"
-PATH="$HOME/Android/flutter/bin:$PATH"
-PATH="$HOME/.gem/ruby/2.5.0/bin:$PATH"
-PATH="/snap/bin:$PATH"
-export PATH
+        # set PATH so it includes user's private bin directories
+        PATH="$HOME/bin:$HOME/.local/bin:$PATH"
+        PATH="$HOME/go/bin:$PATH"
+        PATH="$HOME/.cabal/bin:$PATH"
+        PATH="$HOME/.rbenv/bin:$PATH"
+        PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
+        PATH="$HOME/.rbenv/bin:$PATH"
+        PATH="$HOME/bin:$PATH"
+        PATH="$HOME/.local/bin:$PATH"
+        PATH="$HOME/bin/gyb:$PATH"
+        PATH="$HOME/.cargo/bin:$PATH"
+        PATH="$HOME/Android/flutter/bin:$PATH"
+        PATH="$HOME/.gem/ruby/2.5.0/bin:$PATH"
+        PATH="/snap/bin:$PATH"
+        export PATH
 
-if [ -n "$SSH_CONNECTION" ] && [ -z "$DISPLAY" ]; then
-  export XAUTHORITY="$(ls "/run/user/$(id -u)/.mutter-Xwaylandauth*" 2>/dev/null | head -1)"
-fi
+        if [ -n "$SSH_CONNECTION" ] && [ -z "$DISPLAY" ]; then
+          export XAUTHORITY="$(ls "/run/user/$(id -u)/.mutter-Xwaylandauth*" 2>/dev/null | head -1)"
+        fi
 
-export SOPS_AGE_KEY_CMD="${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i ${config.home.homeDirectory}/.ssh/sops.id_ed25519"
+        export SOPS_AGE_KEY_CMD="${pkgs.ssh-to-age}/bin/ssh-to-age -private-key -i ${config.home.homeDirectory}/.ssh/sops.id_ed25519"
 
-# Make Tab use fzf fuzzy completion instead of needing **
-export FZF_COMPLETION_TRIGGER=""
+        # Make Tab use fzf fuzzy completion instead of needing **
+        export FZF_COMPLETION_TRIGGER=""
     '';
     shellAliases = {
       # Check if xclip is even being used in hyprland
