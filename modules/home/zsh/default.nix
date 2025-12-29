9f2b1b9185d37e6fc3b1ea44012d60bd232860d1
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2025-12-29 14:44:03 +0530

    Map Ctrl-S to search backwards

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -73,6 +73,8 @@
         # bindkey '^R' history-incremental-pattern-search-backward
         # bindkey '^S' history-incremental-pattern-search-forward
 
+        bindkey '^S' history-incremental-pattern-search-backward
+
         autoload edit-command-line
         zle -N edit-command-line
         bindkey -M vicmd ' ' edit-command-line
