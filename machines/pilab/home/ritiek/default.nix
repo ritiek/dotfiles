d7f252218a203d94b1a228435fb768717f3b0d4a
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-01-04 03:47:43 +0530

    Override cd only when zoxide is enabled

--- machines/pilab/home/ritiek/default.nix
+++ machines/pilab/home/ritiek/default.nix
@@ -45,6 +45,7 @@ in
     ./../../../../modules/home/zsh
     ./../../../../modules/home/git
     ./../../../../modules/home/neovim
+    ./../../../../modules/home/zoxide.nix
     # ./../../../../modules/home/zellij.nix
     ./../../../../modules/home/shpool.nix
     ./../../../../modules/home/btop.nix
