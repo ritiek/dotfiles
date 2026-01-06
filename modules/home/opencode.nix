adfa234287a45e95559fdc89ca96ab9701de9385
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-01-06 12:46:10 +0530

    Be rational (somehow!)

--- modules/home/opencode.nix
+++ modules/home/opencode.nix
@@ -418,7 +418,7 @@ in
           prompt = ''
             # Conversational Agent
 
-            Engage in meaningful and context-aware conversations with the user.
+            Engage in meaningful and context-aware conversations with the user. Be rational.
           '';
           tools = {
             write = false;
