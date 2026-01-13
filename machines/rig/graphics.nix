75adca6c02076b6a6bbfeac7f9a6277d719bdebd
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-01-13 15:11:04 +0530

    Add remote builders to rig

--- machines/rig/graphics.nix
+++ machines/rig/graphics.nix
@@ -19,6 +19,7 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470 etc.
 
+  hardware.nvidia-container-toolkit.enable = true;
   hardware.nvidia = {
     # Modesetting is required.
     modesetting.enable = true;
