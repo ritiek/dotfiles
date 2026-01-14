ccff35083cdca5a5560eac03bfb10da9488decbc
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-01-14 18:32:42 +0530

    Use upstream rbw

--- modules/home/rbw.nix
+++ modules/home/rbw.nix
@@ -2,9 +2,9 @@
 {
   programs.rbw = {
     enable = true;
-    package = pkgs.rbw.overrideAttrs (old: {
-      src = inputs.rbw;
-    });
+    # package = pkgs.rbw.overrideAttrs (old: {
+    #   src = inputs.rbw;
+    # });
     settings = {
       email = "ritiekmalhotra123@gmail.com";
       # base_url = "https://vaultwarden.clawsiecats.lol";
