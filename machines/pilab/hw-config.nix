21d8098a40f29d606e3240062179313ce92c688a
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-03-15 15:45:07 +0530

    Enable BTRFS auto-scrubbing on PiLab

--- machines/pilab/hw-config.nix
+++ machines/pilab/hw-config.nix
@@ -164,6 +164,13 @@ in
     };
   };
 
+  services.btrfs.autoScrub = {
+    enable = true;
+    fileSystems = [
+      "/media/HOMELAB_MEDIA"
+    ];
+  };
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
