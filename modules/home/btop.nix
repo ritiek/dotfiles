63c1be5abef126098a90957ecb5271cd8e46b45c
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-03-21 03:49:20 +0530

    Try out dracula on btop

--- modules/home/btop.nix
+++ modules/home/btop.nix
@@ -3,7 +3,8 @@
   programs.btop = {
     enable = true;
     settings = {
-      color_theme = "adapta";
+      # color_theme = "adapta";
+      color_theme = "dracula";
       vim_keys = true;
       cpu_graph_lower = "iowait";
       proc_sorting = "memory";
