f96624ea3e544e6fd375e6001d3a1eb12193c13f
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-03-21 05:24:46 +0530

    Tune netbird config

--- modules/netbird.nix
+++ modules/netbird.nix
@@ -1,15 +1,29 @@
 { pkgs, config, ... }:
 {
-  sops.secrets."netbird.setupkey" = {};
+  sops.secrets."netbird.setupkey" = {
+    owner = config.services.netbird.clients.birdnet.user.name;
+    group = config.services.netbird.clients.birdnet.user.group;
+  };
+
+  # systemd.tmpfiles.rules = [
+  #   "d /var/lib/netbird-birdnet/.config 0700 ${config.services.netbird.clients.birdnet.user.name} ${config.services.netbird.clients.birdnet.user.group} -"
+  # ];
 
   services.netbird = {
     enable = true;
     ui.enable = config.hardware.graphics.enable;
     clients.birdnet = {
+      # user = {
+      #   name = "root";
+      #   group = "root";
+      # };
       login = {
         enable = true;
         setupKeyFile = config.sops.secrets."netbird.setupkey".path;
+        systemdDependencies = [ "run-secrets.d.mount" ];
       };
+      openFirewall = true;
+      openInternalFirewall = true;
       port = 51840;
     };
   };
