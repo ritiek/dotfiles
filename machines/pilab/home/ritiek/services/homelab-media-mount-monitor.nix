2c6d23e2115fa9e49f09842dd46f54d9f0c005f5
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-03-24 01:31:08 +0530

    Fix Blue LED logic conflict mounts vs backups

--- machines/pilab/home/ritiek/services/homelab-media-mount-monitor.nix
+++ machines/pilab/home/ritiek/services/homelab-media-mount-monitor.nix
@@ -1,31 +1,24 @@
 { config, pkgs, lib, ... }:
 
 let
-  homelab-media-mount-monitor-loop = pkgs.writeShellScriptBin "homelab-media-mount-monitor-loop" ''
+  blink-blue-led = pkgs.writeShellScriptBin "blink-blue-led" ''
     while true; do
-      if ! ${pkgs.util-linux}/bin/mountpoint -q /media/HOMELAB_MEDIA; then
-        ${pkgs.libgpiod}/bin/gpioset -t 0 -c gpiochip0 4=1
-        ${pkgs.coreutils}/bin/sleep 2
-        ${pkgs.libgpiod}/bin/gpioset -t 0 -c gpiochip0 4=0
-        ${pkgs.coreutils}/bin/sleep 2
-      else
-        ${pkgs.libgpiod}/bin/gpioset -t 0 -c gpiochip0 4=0
-        ${pkgs.coreutils}/bin/sleep 2
-      fi
+      ${pkgs.libgpiod}/bin/gpioset -t 0 -c gpiochip0 4=1
+      ${pkgs.coreutils}/bin/sleep 2
+      ${pkgs.libgpiod}/bin/gpioset -t 0 -c gpiochip0 4=0
+      ${pkgs.coreutils}/bin/sleep 2
     done
   '';
 in
 {
   systemd.user.services.homelab-media-mount-monitor = {
     Unit = {
-      Description = "Mount monitor for HOMELAB_MEDIA on Blue LED";
+      Description = "Blue LED blink indicator for HOMELAB_MEDIA not mounted";
       After = [ "local-fs.target" ];
     };
     Service = {
       Type = "simple";
-      Restart = "on-failure";
-      RestartSec = "10s";
-      ExecStart = "${homelab-media-mount-monitor-loop}/bin/homelab-media-mount-monitor-loop";
+      ExecStart = "${blink-blue-led}/bin/blink-blue-led";
       ExecStopPost = "${pkgs.libgpiod}/bin/gpioset -t 0 -c gpiochip0 4=0";
     };
     Install = {
