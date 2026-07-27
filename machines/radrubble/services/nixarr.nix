4de23f6d5481244482c09e48e27fe4e08cd81e35
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-07-27 23:49:10 +0530

    Add notify scripts to qBittorrent

--- machines/radrubble/services/nixarr.nix
+++ machines/radrubble/services/nixarr.nix
@@ -1,5 +1,70 @@
 { config, lib, pkgs, everythingElsePath, ... }:
 
+let
+  qbNotifyScript = pkgs.writeShellScriptBin "qbittorrent-notify" ''
+    #!/bin/sh
+    set -eu
+
+    GOTIFY_URL="http://pilab.lion-zebra.ts.net:8893"
+    GOTIFY_TOKEN="AieV3s6h-PYmsGy"
+    PRIORITY=3
+
+    TITLE="$1"
+    TORRENT_NAME="''${2:-%N}"
+    CATEGORY="''${3:-%L}"
+    TAGS="''${4:-%G}"
+    CONTENT_PATH="''${5:-%F}"
+    SAVE_PATH="''${6:-%D}"
+    FILE_COUNT="''${7:-%C}"
+    TORRENT_SIZE="''${8:-%Z}"
+    TRACKER="''${9:-%T}"
+    INFO_HASH_V1="''${10:-%I}"
+
+    human_readable_size() {
+        bytes=$1
+        case "$bytes" in
+            -1) echo "Unknown"; return ;;
+            ''|*[!0-9-]*) echo "$bytes"; return ;;
+            -*) echo "Unknown"; return ;;
+        esac
+        index=0
+        size=$bytes
+        while [ $size -ge 1024 ] && [ $index -lt 4 ]; do
+            size=$((size / 1024))
+            index=$((index + 1))
+        done
+        case $index in
+            0) echo "''${size} B" ;;
+            1) echo "''${size} KB" ;;
+            2) echo "''${size} MB" ;;
+            3) echo "''${size} GB" ;;
+            4) echo "''${size} TB" ;;
+        esac
+    }
+
+    format_message() {
+        size_human=$(human_readable_size "$TORRENT_SIZE")
+        msg="Name: $TORRENT_NAME\n"
+        [ "$TORRENT_SIZE" != "-1" ] && msg="''${msg}Size: $size_human\n"
+        [ "$FILE_COUNT" != "-1" ] && msg="''${msg}Files: $FILE_COUNT\n"
+        [ -n "$CATEGORY" ] && [ "$CATEGORY" != "N/A" ] && [ "$CATEGORY" != "%L" ] && msg="''${msg}Category: $CATEGORY\n"
+        [ -n "$TAGS" ] && [ "$TAGS" != "N/A" ] && [ "$TAGS" != "%G" ] && msg="''${msg}Tags: $TAGS\n"
+        msg="''${msg}Save Path: $SAVE_PATH\n"
+        [ -n "$TRACKER" ] && [ "$TRACKER" != "N/A" ] && [ "$TRACKER" != "%T" ] && msg="''${msg}Tracker: $TRACKER\n"
+        hash_short=$(echo "$INFO_HASH_V1" | cut -c1-16)
+        msg="''${msg}Info Hash: ''${hash_short}..."
+        printf "%b" "$msg"
+    }
+
+    MESSAGE=$(format_message)
+    curl -s -X POST "$GOTIFY_URL/message?token=$GOTIFY_TOKEN" \
+        -F "title=$TITLE" \
+        -F "message=$MESSAGE" \
+        -F "priority=$PRIORITY" > /dev/null
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] Notification sent for: $TORRENT_NAME" >&2
+'';
+in
+
 # Migrated from pilab's docker/oci-containers *arr stack. EVERYTHING_ELSE is
 # the same LUKS-encrypted btrfs drive that used to be plugged into pilab,
 # containing the existing Radarr/Sonarr/Bazarr/Prowlarr/qBittorrent/Jellyfin/
@@ -67,6 +132,12 @@ in
         Preferences = {
           "WebUI\\Username" = "ritiek";
           "WebUI\\Password_PBKDF2" = "@ByteArray(cUIyMDI0Rml4ZWRTYWx0IQ==:80h/fCeBkAZDlBBCKCO0KWakgR4i6Lb0oFSSWUh/SHl8v71Yh/kLq3itEuqdopZhluGSIJsZmNeYwgDa4t03lA==)";
+        };
+        AutoRun = {
+          "OnTorrentAdded\\Enabled" = true;
+          "OnTorrentAdded\\Program" = "${qbNotifyScript}/bin/qbittorrent-notify Download_Added %N %L %G %F %D %C %Z %T %I";
+          "OnTorrentFinished\\Enabled" = true;
+          "OnTorrentFinished\\Program" = "${qbNotifyScript}/bin/qbittorrent-notify Download_Finished %N %L %G %F %D %C %Z %T %I";
         };
       };
     };
