b9320e56e0d3031a9a10bba4b8ef95a07cddeb8d
Ritiek Malhotra <ritiekmalhotra123@gmail.com>
2026-08-01 01:25:14 +0530

    generators/minimal: use shared substituters list

--- generators/minimal.nix
+++ generators/minimal.nix
@@ -1,4 +1,7 @@
 { inputs, ... }:
+let
+  cache = import ../substituters.nix;
+in
 {
   # TODO: Increase tmpfs mount size on /nix/.rw-store
 
@@ -64,16 +67,9 @@
 
   nix.settings = {
     auto-optimise-store = true;
-    substituters = [
-      "https://nix-community.cachix.org"
-      "https://cache.garnix.io"
-      "https://hyprland.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-    ];
+    inherit (cache)
+      substituters
+      trusted-public-keys;
   };
 
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
